3c0a87c3b674c55dff561418636a1d9d3dac973c
WEBSERVER\henry <henryma14@gmail.com>
2025-09-22 13:24:41 -0700

    refactor: on server

--- backend/start_server.bat
+++ backend/start_server.bat
@@ -1,8 +1,8 @@
 @echo off
 
 echo Activating virtual environment...
-call G:\BrightdataSnapshot\backend\.venv\Scripts\activate
+call G:\WebScrapersApp\backend\.venv\Scripts\activate
 
 echo Starting Waitress server...
-uvicorn main:app --host agent-back.lottotry.com --port 9000
+run uvicorn main:app --host agent-back.lottotry.com --port 9000
 pause
